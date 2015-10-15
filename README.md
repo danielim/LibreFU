bce1c8223fc5b107bb211cfb2c336537d7cece4e
Daniel Chen <chen@daniel.im>
2015-10-15 00:21:05 -0500

    working simple file uploader

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LibreFU
 File Uploader/Manager built with JS*
+
+##Started with tutorial by [Shahid Shaikh](https://codeforgeek.com/about/) linked below.
+
+[File Uploads using NodeJS](https://codeforgeek.com/2014/11/file-uploads-using-node-js/)
---
+++ server.js
@@ -0,0 +1,51 @@
+// Define dependencies.
+
+var express = require("express");
+var multer = require("multer");
+var app = express();
+var port = 3000;
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        "use strict";
+        cb(null, "./uploads/");
+    },
+    filename: function(req, file, cb) {
+        "use strict";
+        var filenamearray = file.originalname.split(".");
+        if(filenamearray[1]) {
+            var fileext = filenamearray.pop();
+            cb(null, filenamearray.join() + "-" + Date.now() + "." + fileext);
+        } else {
+            cb(null, filenamearray.join() + "-" + Date.now());
+        }
+    }
+});
+
+var upload = multer({ storage: storage}).single("filearchive");
+
+// Handling routes.
+
+app.get("/", function(req, res){
+    "use strict";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/api/single", function(req, res){
+    "use strict";
+    upload(req, res, function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        console.log(req.file.filename);
+        var fileUrl = "../" + req.file.filename;
+        res.send("<a href='" + fileUrl + "'>File uploaded successfully.</a>");
+    });
+});
+
+// Run the File Upload Server.
+
+app.listen(port, function(){
+    "use strict";
+    console.log("Working on port " + port);
+});
